1b47013861bf3713d23099bd007d1c43c41523e5
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-08-28 16:54:22 -0600

    friday wrap styling next feture compleate

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -9,21 +9,21 @@ export default class Task {
 
     get Template() {
         return `
-        <div class="card bg-warning offset-1 col-3">
+        <div class="card bg-warning m-2 col-3 task-body d-flex flex-column justify-content-around">
         <div class="card text-light d-flex flex-column" style="background-color: ${this.color}">
-        <i class="fas fa-times text-danger align-self-end" onclick="app.tasksController.removeTask('${this.id}')"></i>
+        <i class="fas fa-times text-danger align-self-end m-1" onclick="app.tasksController.removeTask('${this.id}')"></i>
         <h1 class="align-self-center">${this.title}</h>
         </div>
-        <ol>
+        <ul>
         ${this.SubtaskTemplate}
-        </ol>
-        <div>
-        <form class="form-inline row" onsubmit="app.tasksController.newSubTask(event, '${this.id}')">
+        </ul>
+        <div class="taskform">
+        <form class="form-inline" onsubmit="app.tasksController.newSubTask(event, '${this.id}')">
                 <div class="form-group">
                     <label class="mr-1" for="title">task</label>
                     <input type="text" name="subtask" id="subtask-${this.id}" class="form-control" placeholder="..." >
                 </div>
-                <button type="submit" class="btn btn-outline-primary"></button>
+                <button type="submit" class="btn btn-outline-primary">New item</button>
             </form>
             </div>
             </div>
